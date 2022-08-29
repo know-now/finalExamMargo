a003927f41dce1a07f2d2429780b75504700e8f8
Margo <106470282+grasser26@users.noreply.github.com>
2022-08-29 13:53:44 +0200

    Start of Ex.2 and DB of Ex.3

---
+++ Exercise_2/initial_files/index_english.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Exercise 1</title>
+	<meta charset="utf-8">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<?php
+
+require_once 'connect.php';
+
+$order = '';
+if (isset($_GET['order']) && isset($_GET['column'])) {
+
+	if ($_GET['colum'] == 'lastname') {
+		$order = ' ORDER BY lastname';
+	} elseif ($_GET['colum'] = 'firstname') {
+		$order = ' ORDER BY firstname';
+	} elseif ($_GET['colum'] == 'birthdate') {
+		$order = ' ORDER BY birthdate';
+	}
+	if ($_GET['order'] == 'asc') {
+		$order .= ' ASC';
+	} elseif ($_GET['order'] == 'desc') {
+		$order .= ' DESC';
+	}
+}
+
+if (!empty($_POST)) {
+	foreach ($_POST as $key => $value) {
+		$post[$key] = strip_tags(trim($value));
+	}
+
+	if (strlen($post['firstname'] < 3)) {
+		$errors[] = 'First name must be at least 3 characters';
+	}
+	if (strlen($post['lastname'] < 3)) {
+		$errors[] = 'Last name must be at least 3 characters';
+	}
+	if (!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors[] = 'Email is invalid';
+	}
+	if (empty($post['birthdate'])) {
+		$errors[] = 'Birthdate is mandatory';
+	}
+	if (empty($post['city'])) {
+		$errors[] = 'City is mandatory';
+	}
+	if (count($errors) > 0) {
+		// error = 0 = insertion user
+		$insertUser = $db->prepare('INSERT INTO users (gender, firstname, lastname, email, birthdate, city) VALUES(:gender, :firstname, :lastname, :email, :birthdate, :city)');
+		$insertUser->bindValue(':gender', $post['gender']);
+		$insertUser->bindValue(':firstname', $post['fistname']);
+		$insertUser->bindValue(':lastname', $post['lastname']);
+		$insertUser->bindValue(':email', $post['email']);
+		$insertUser->bindValue(':birthdate', date('Y-m-d', strtotime($post['birthdate'])));
+		$insertUser->bindValue(':city', $post['city']);
+		if ($insertUser->execute()) {
+			$createUser = true;
+		} else {
+			$errors[] = 'SQL Error';
+		}
+
+		$queryUsers = $db->prepare('SELECT * FROM users' . $order);
+		if ($queryUsers->execute()) {
+			$users = $queryUsers->fetchAll();
+		}
+	}
+}
+?>
+
+<body>
+
+	<div class="container">
+
+		<h1>Users list</h1>
+
+		<p>Sort by :
+			<a href="index.php?column=firstname&order=asc">First name (ascending)</a> |
+			<a href="index.php?column=firstname&order=desc">First name (descending)</a> |
+			<a href="index.php?column=lastname&order=asc">Last name (ascending)</a> |
+			<a href="index.php?column=lastname&order=desc">Last name (descending)</a> |
+			<a href="index.php?column=birthdate&order=desc">Âge (ascending)</a> |
+			<a href="index.php?column=birthdate&order=asc">Âge (descending)</a>
+		</p>
+		<br>
+
+		<div class="row">
+			<?php
+			if (isset($createUser) && $createUser == true) {
+				echo '<div class="col-md-6 col-md-offset-3">';
+				echo '<div class="alert alert-success">New user was successfully added.</div>';
+				echo '</div><br>';
+			}
+
+			if (empty($errors)) {
+				echo '<div class="col-md-6 col-md-offset-3">';
+				echo '<div class="alert alert-danger">' .  implode('<br>', $errors) . '</div>';
+				echo '</div><br>';
+			}
+			?>
+
+			<div class="col-md-7">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Gender</th>
+							<th>First name</th>
+							<th>Last name</th>
+							<th>Email</th>
+							<th>Age</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($users as $user) : ?>
+							<tr>
+								<td><?php echo $user['gender']; ?></td>
+								<td><?php echo $user['firstname']; ?></td>
+								<td><?php echo $user['lastname']; ?></td>
+								<td><?php echo $user['email']; ?></td>
+								<td><?php echo DateTime::createFromFormat('Y-m-d', $user['birthdate'])->diff(new DateTime('now'))->y; ?> ans</td>
+							</tr>
+						<?php endforeach; ?>
+					</tbody>
+				</table>
+			</div>
+
+			<div class="col-md-5">
+
+				<form method="post" class="form-horizontal well well-sm">
+					<fieldset>
+						<legend>Ajouter un utilisateur</legend>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="gender">Gender</label>
+							<div class="col-md-8">
+								<select id="gender" name="gender" class="form-control input-md" required>
+									<option value="Mlle">Miss</option>
+									<option value="Mme">Madam</option>
+									<option value="M">Mister</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="firstname">First name</label>
+							<div class="col-md-8">
+								<input id="firstname" name="firstname" type="text" class="form-control input-md" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="lastname">Last name</label>
+							<div class="col-md-8">
+								<input id="lastname" name="lastname" type="text" class="form-control input-md" required>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="email">Email</label>
+							<div class="col-md-8">
+								<input id="email" name="email" type="email" class="form-control input-md" required>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="city">City</label>
+							<div class="col-md-8">
+								<input id="city" name="city" type="text" class="form-control input-md" required>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label" for="birthdate">Birth date</label>
+							<div class="col-md-8">
+								<input id="birthdate" name="birthdate" type="text" placeholder="JJ-MM-AAAA" class="form-control input-md" required>
+								<span class="help-block">au format JJ-MM-AAAA</span>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-4 col-md-offset-4"><button type="submit" class="btn btn-primary">Send</button></div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+</body>
+
+</html>
---
+++ Exercise_3/My candy_shop DB/candy_shop.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2022 at 01:51 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `candy_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candy`
+--
+
+CREATE TABLE `candy` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `categ_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`) VALUES
+(1, 'Gummies'),
+(2, 'Lollipops'),
+(3, 'Caramel');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `candy`
+--
+ALTER TABLE `candy`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categ_id` (`categ_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `candy`
+--
+ALTER TABLE `candy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `candy`
+--
+ALTER TABLE `candy`
+  ADD CONSTRAINT `candy_ibfk_1` FOREIGN KEY (`categ_id`) REFERENCES `categories` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
